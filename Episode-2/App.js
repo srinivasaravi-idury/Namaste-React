548cbb8ae46d9d8243d3cd5db6a6c446865f3b8a
srinivasaravi-idury <122342523+srinivasaravi-idury@users.noreply.github.com>
2024-10-21 23:27:00 +0530

    episode-2 igniting the app

---
+++ Episode-2/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+const heading = React.createElement("h1",{id:"heading"},"Namste ");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading)
