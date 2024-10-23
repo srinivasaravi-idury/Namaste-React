256c385d8d5f22f2c5ed2e28c3c8fa2cf159d19a
srinivasaravi-idury <122342523+srinivasaravi-idury@users.noreply.github.com>
2024-10-23 23:06:23 +0530

    renamed folder "Episode-2" to "react-application"

--- Episode-1/App.js
+++ Episode-1/App.js
@@ -1,3 +1,6 @@
-const heading = React.createElement("h1",{id:"heading"},"Hello World from React!");
+const heading = React.createElement("div",{},[
+    React.createElement('h1',{},"Hello world from H1"),
+    React.createElement('h2',{},"Hello world from H2")
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading)
